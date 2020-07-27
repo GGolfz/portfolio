cc1a3473bd0554bdbc7cde069601718a53ec8395
GGolfz <wisarutgolf11@gmail.com>
2020-07-28 00:09:06 +0700

    [INIT] Project && [CONFIG] Build & Deploy

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <article><h1>{{ data.title }}</h1>
+        <p>{{ data.description }}</p>
+    <nuxt-content :document="data" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const data = await $content('blog', params.slug).fetch()
+
+    return { data }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+      BLOG
+  </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
