6c8563147c8cc30dfcdf0e6bd2639be2259c0408
GGolfz <wisarutgolf11@gmail.com>
2020-07-28 19:03:47 +0700

    [ADD] Nav for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <Nav :dark="val" />
+    <Nav :dark="val" class="desktop"/>
+    <NavMobile :dark="val" class="mobile"/>
       <div :class="val?'light toggle':'dark toggle'">
-        <svg v-if="val" @click="val=!val" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg v-if="val" @click="val=!val" xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z"/>
           <circle cx="12" cy="12" r="4" />
           <path d="M3 12h1M12 3v1M20 12h1M12 20v1M5.6 5.6l.7 .7M18.4 5.6l-.7 .7M17.7 17.7l.7 .7M6.3 17.7l-.7 .7" />
         </svg>
-        <svg v-else @click="val=!val" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg v-else @click="val=!val" xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z"/>
           <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" />
         </svg>
@@ -18,9 +19,10 @@
 
 <script>
 import Nav from '../components/Nav'
+import NavMobile from '../components/NavMobile'
 export default {
   components:{
-    Nav
+    Nav,NavMobile
   },
   data() {
     return {
@@ -30,6 +32,24 @@ export default {
 }
 </script>
 <style>
+@media only screen and (max-width: 600px) {
+  .desktop{
+    display: none;
+  }
+  .mobile {
+    display: block;
+  }
+}
+@media only screen and (min-width: 601px) {
+  .desktop{
+    display: flex;
+  }
+  .mobile {
+    display: none;
+  }
+  
+}
+
 html {
   font-family: 'Montserrat', sans-serif, 'Titillium Web', sans-serif;
 }
@@ -41,8 +61,8 @@ html {
 }
 .toggle {
   width: fit-content;
-  position: absolute;
-  top: 0;
+  position: fixed;
+  bottom: 0;
   right: 0;
   margin: 1.5%;
   border-radius:50px;
@@ -50,6 +70,7 @@ html {
   display: flex;
   align-items: center;
   justify-items: center;
+  cursor:pointer;
 }
 .light {
   background: #ffcc00ee;
