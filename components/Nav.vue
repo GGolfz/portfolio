6c9ead49d0d55c0cd7ece350d79534969f2d34a4
GGolfz <wisarutgolf11@gmail.com>
2020-07-28 17:09:02 +0700

    [ADD] Default Active

--- components/Nav.vue
+++ components/Nav.vue
@@ -7,7 +7,7 @@
     :background-color="dark?'#212121':'#efefef'"
     :default-active="current"
   >
-    <el-menu-item style="padding:0%" index="1">
+    <el-menu-item style="padding:0%" index="1" @click="goto('Home')">
       <img src="logo.png" alt="logo" height="100%" style="transform: scaleX(-1);">
     </el-menu-item>
     <el-menu-item
@@ -15,6 +15,7 @@
       :key="index"
       :index="`` + (index + 2)"
       @click="goto(item)"
+      :class="isHome && item== 'Home'? 'is-active':''"
     >
       <nuxt-link :to="`/${item == 'Home' ? '' : item.toLowerCase()}`"
         >{{ item }}
@@ -29,6 +30,7 @@ export default {
     return {
       menu: ['Home', 'About', 'Blog', 'Award', 'Project', 'Contact'],
       current: '',
+      isHome: true
     }
   },
   props: {
@@ -38,6 +40,10 @@ export default {
     goto(href) {
       if (href == 'Home') {
         href = ''
+        this.isHome = true
+      }
+      else{
+        this.isHome = false
       }
       this.$router.push({ path: '/' + href.toLowerCase() })
     },
@@ -46,10 +52,12 @@ export default {
     const path = this.$route.name
     if(path == 'index'){
       this.current = '2'
+      this.isHome = true
     } else {
       await this.menu.forEach((el, index) => {
         if (el.toLowerCase() == path) {
           this.current = `` + (index + 2)
+          this.isHome = false
         }
       })
     }
@@ -85,10 +93,12 @@ a {
   padding-bottom: 5px;
   border-bottom: 2px #CBB7A6 solid;
   font-weight: 600;
+  color: #2F2F2F;
 }
 .dark.el-menu--horizontal > .el-menu-item.is-active > a {
   padding-bottom: 5px;
   border-bottom: 2px #c99d78 solid;
   font-weight: 600;
+  color: #f8f8f8;
 }
 </style>
