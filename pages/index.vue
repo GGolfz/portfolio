6c33cc849385f1f1f0997471ba5fe73896473e93
GGolfz <wisarutgolf11@gmail.com>
2020-07-30 01:02:26 +0700

    [ADD] Background for show case page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="dark?'container dark-theme':'container light-theme'">
     <div class="banner">
-      HOME PAGE - sticky with nowrap
+      HOME PAGE - waiting for idea too.
     </div>
   </div>
 </template>
