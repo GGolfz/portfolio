d6856e4fd5d0c5c29ecb8574a369b3ca6d7c1699
GGolfz <wisarutgolf11@gmail.com>
2020-10-10 01:33:32 +0700

    Update Sitemap build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,152 +1,174 @@
 const fs = require('fs')
 const routes = []
-const urlroutes = []
-const getRoutes = async () =>{
-  await fs.readdir('./content/',async (err,dirs)=>{
-      for(let i of dirs){
-          await fs.readdir('./content/'+i,(err,files)=>{
-              for(let j of files){
-                  let route = j.split('.')
-                  routes.push('/'+i+'/'+route[0])
-                  urlroutes.push({url:'/'+i+'/'+route[0]})
-              }
-          })
-      }
+const getRoutes = async () => {
+  await fs.readdir('./content/', async (err, dirs) => {
+    for (let i of dirs) {
+      await fs.readdir('./content/' + i, (err, files) => {
+        for (let j of files) {
+          let route = j.split('.')
+          routes.push('/' + i + '/' + route[0])
+        }
+      })
+    }
   })
 }
 getRoutes()
-
 export default {
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
   mode: 'universal',
-  router:{
-    base:'/portfolio',
+  router: {
+    base: '/portfolio',
   },
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
   target: 'static',
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     title: process.env.npm_package_name || 'GGolfz',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'google-site-verification', content: 'm1xMiv2eLLg484iC2NZL_PxiySQEL-N3LhyGev6ZlaU'},
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || 'GGolfz\'s portfolio' }
+      {
+        name: 'google-site-verification',
+        content: 'm1xMiv2eLLg484iC2NZL_PxiySQEL-N3LhyGev6ZlaU',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || "GGolfz's portfolio",
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/*', href: 'logo.png' },
-      { rel: "preconnect", href: "https://cdn.jsdelivr.net"},
-      { rel: "dns-prefetch", href: "https://cdn.jsdelivr.net"},
+      { rel: 'preconnect', href: 'https://cdn.jsdelivr.net' },
+      { rel: 'dns-prefetch', href: 'https://cdn.jsdelivr.net' },
       {
-        rel: "preload",
-        as: "style",
-        href: "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css",
+        rel: 'preload',
+        as: 'style',
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css',
         integrity:
-          "sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq",
-        crossorigin: "anonymous",
+          'sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq',
+        crossorigin: 'anonymous',
         onload: "this.onload=null;this.rel='stylesheet'",
-        defer: true
+        defer: true,
       },
-      { rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Montserrat&family=Titillium+Web:wght@400;600&display=swap', defer: true },
-      { rel: 'stylesheet', href:'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css', defer: true}
-    ]
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Montserrat&family=Titillium+Web:wght@400;600&display=swap',
+        defer: true,
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css',
+        defer: true,
+      },
+    ],
   },
   /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+   ** Global CSS
+   */
+  css: ['element-ui/lib/theme-chalk/index.css'],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: ['@/plugins/element-ui'],
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
   components: true,
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt/content
     '@nuxt/content',
-    ['@nuxtjs/google-analytics', {
-      id: process.env.GA_ID || ''
-    }],
-    "nuxt-compress",
-    "@nuxtjs/sitemap"
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: process.env.GA_ID || '',
+      },
+    ],
+    'nuxt-compress',
+    '@nuxtjs/sitemap',
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {},
   /*
-  ** Content module configuration
-  ** See https://content.nuxtjs.org/configuration
-  */
+   ** Content module configuration
+   ** See https://content.nuxtjs.org/configuration
+   */
   content: {
     markdown: {
       remarkPlugins: () => [
-        "remark-squeeze-paragraphs",
-        "remark-slug",
-        "remark-external-links",
-        "remark-math",
-        "remark-attr",
-        "~/plugins/remark-sub-super-modified.js"
+        'remark-squeeze-paragraphs',
+        'remark-slug',
+        'remark-external-links',
+        'remark-math',
+        'remark-attr',
+        '~/plugins/remark-sub-super-modified.js',
       ],
-      rehypePlugins: ["rehype-katex"]
-    }
-  },
-  sitemap: {
-    hostname: "https://ggolfz.github.io/portfolio",
-    routes: [
-      { url: "/", changefreq: "daily", priority: 1 },
-      { url: "/about" },
-      { url: "/contacts" },
-      ...urlroutes
-    ],
-    defaults: {
-      changefreq: "monthly",
-      priority: 0.8
-    }
+      rehypePlugins: ['rehype-katex'],
+    },
   },
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
   build: {
     transpile: [/^element-ui/],
     publicPath: '/nuxt-dist',
     filenames: {
-      chunk: '[id].[contenthash].js'
-    }
+      chunk: '[id].[contenthash].js',
+    },
+  },
+  sitemap: {
+    hostname: 'https://ggolfz.github.io/',
+    routes: () => {
+      let route = [
+        { url: '/', changefreq: 'daily', priority: 1 },
+        { url: '/about', priority: 0.9 },
+        { url: '/contact', priority: 0.9 },
+        { url: '/resume', priority: 0 },
+        '/blog',
+        '/showcase',
+        '/showcase/award',
+        '/showcase/project',
+      ]
+      for (let i of routes) {
+        route.push(i)
+      }
+      return route
+    },
+    defaults: {
+      changefreq: 'monthly',
+      priority: 0.8,
+    },
   },
   generate: {
-    routes: routes
-  }
+    routes: routes,
+  },
 }
